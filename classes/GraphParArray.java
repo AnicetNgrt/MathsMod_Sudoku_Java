d04a6b7e5181442439a5da876f58fa0cfbdd0900
Anicet Nougaret <anicet.nougaret@zohomail.eu>
2020-10-14 05:59:17 +0000

    fixed tab

--- classes/GraphParArray.java
+++ classes/GraphParArray.java
@@ -8,7 +8,7 @@ public class GraphParArray implements Graph {
     public ArrayList<Integer[]> liaisons = new ArrayList();
 
     // Pire cas O(n), meilleur cas O(1), cas moyen O(n)
-	public sontConnectés(int sommet1, int sommet2) {
+		public sontConnectés(int sommet1, int sommet2) {
         for(int[] liaison: liaisons) {
             if(liaison[0] == sommet1 && liaison[1] == sommet2) return true;
             if(liaison[1] == sommet1 && liaison[0] == sommet2) return true;
@@ -17,19 +17,19 @@ public class GraphParArray implements Graph {
     }
 
     // O(1)
-	public connecterBinaire(int sommet1, int sommet2) {
+		public connecterBinaire(int sommet1, int sommet2) {
         liaisons.add(new Integer[] {sommet1, sommet2});
     }
 
     // O(1) pour chaque sommet
-	connecterMultiple(int sommet, int[] sommets) {
+		public connecterMultiple(int sommet, int[] sommets) {
         for(int s:sommets) {
             connecterBinaire(sommet, s);
         }
     }
 
-	// O(n)
-	public int[][] listeLiaisons() {
+		// O(n)
+		public int[][] listeLiaisons() {
         int[][] liaisons = new int[this.liaisons.size()][2];
         for(int i = 0; i < this.liaisons.size(); i++) {
             liaisons[i][0] = this.liaisons.get(i)[0];
@@ -38,7 +38,7 @@ public class GraphParArray implements Graph {
         return liaisons;
     }
 
-	public int[] listeSommets() {
+		public int[] listeSommets() {
         int[] sommets;
         return sommets;
     }
